6c339b004c72b4cef997544f537bcb97cb7a1f92
Galeeb <shaikgaleebbasha@gmail.com>
2020-08-09 21:45:06 +0530

    Enable Django Admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -38,7 +38,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserProfileManager()
+    objects = UserProfileManager() # To override the base Manager
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
